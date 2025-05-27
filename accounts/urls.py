3713bd11c7017615613ad18e4eeadacd851af03f
DevLucasEduardo <lucaseduardodev@outlook.com>
2025-05-26 23:47:49 -0300

    all http methods

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ClientList, AccountList
+from .views import ClientList, AccountList, ClientDetail, AccountDetail
 
 urlpatterns = [
     path('clients/', ClientList.as_view(), name='client-list'),
+    path('clients/<int:pk>', ClientDetail.as_view(), name='client-detail'),
     path('accounts/', AccountList.as_view(), name='account-list'),
-    path('accounts/<int:pk>', AccountList.as_view(), name='account-detail'),
+    path('accounts/<int:pk>', AccountDetail.as_view(), name='account-detail'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,14 @@
 from rest_framework.views import APIView
-from .models import Client, Account
-from .serializers import ClientSerializer, AccountSerializer
 from rest_framework.response import Response
 from rest_framework import status
+from .models import Client, Account
+from .serializers import ClientSerializer, AccountSerializer
 
 
 class ClientList(APIView):
     def get(self, request, format=None):
-        client = Client.objects.all()
-        serializer = ClientSerializer(client, many=True)
+        clients = Client.objects.all()
+        serializer = ClientSerializer(clients, many=True)
         return Response(serializer.data)
 
     def post(self, request):
@@ -16,14 +16,46 @@ class ClientList(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+
+
+class ClientDetail(APIView):
+    def get(self, request, pk, format=None):
+        try:
+            client = Client.objects.get(pk=pk)
+        except Client.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        serializer = ClientSerializer(client)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        try:
+            client = Client.objects.get(pk=pk)
+        except Client.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        serializer = ClientSerializer(client, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        try:
+            client = Client.objects.get(pk=pk)
+        except Client.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        client.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
 class AccountList(APIView):
     def get(self, request, format=None):
-        account = Account.objects.all()
-        serializer = AccountSerializer(account, many=True)
-
+        accounts = Account.objects.all()
+        serializer = AccountSerializer(accounts, many=True)
         return Response(serializer.data)
 
     def post(self, request):
@@ -31,11 +63,20 @@ class AccountList(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    def put(self, request, pk):
-        
+
+
+class AccountDetail(APIView):
+    def get(self, request, pk, format=None):
+        try:
+            account = Account.objects.get(pk=pk)
+        except Account.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        serializer = AccountSerializer(account)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
         try:
             account = Account.objects.get(pk=pk)
         except Account.DoesNotExist:
@@ -44,5 +85,14 @@ class AccountList(APIView):
         serializer = AccountSerializer(account, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        try:
+            account = Account.objects.get(pk=pk)
+        except Account.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        account.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
